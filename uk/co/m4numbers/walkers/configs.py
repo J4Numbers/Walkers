1c4a58f0d1d6aaa1dbc50a017f2187cac2316534
Matthew D. Ball <m.d.ball2@ncl.ac.uk>
2015-05-26 21:18:36 +0100

    Configuration file with points where stuff should be entered

---
+++ uk/co/m4numbers/walkers/configs.py
@@ -0,0 +1,8 @@
+__author__ = 'Cynical'
+
+__username__        = "reddit_username"
+__password__        = "reddit_password"
+__reddit_client__   = 'reddit_client_oauth_id'
+__reddit_secret__   = 'reddit_secret_oauth_id'
+__imgur_client__    = 'imgur_client_oauth_id'
+__imgur_secret__    = 'imgur_secret_oauth_id'
