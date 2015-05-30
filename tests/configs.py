518ba9f88ac7ecd8433c7759aea6796227f58ea4
Matthew D. Ball <m.d.ball2@ncl.ac.uk>
2015-05-30 15:21:26 +0100

    Shifted around the config file properly. Also screw this malarkey, will alter the index to make sure I don't have to screw with the ignore again

---
+++ tests/configs.py
@@ -0,0 +1,14 @@
+__author__ = 'Cynical'
+
+__reddit_username__ = "reddit_username"
+__reddit_password__ = "reddit_password"
+__reddit_client__ = 'reddit_client_oauth_id'
+__reddit_secret__ = 'reddit_secret_oauth_id'
+
+__imgur_client__ = 'imgur_client_oauth_id'
+__imgur_secret__ = 'imgur_secret_oauth_id'
+
+__twitter_con_key__ = 'twitter_consumer_key'
+__twitter_con_secret__ = 'twitter_consumer_secret'
+__twitter_acc_token__ = 'twitter_access_token_public'
+__twitter_acc_secret__ = 'twitter_access_token_secret'
--- walkers/configs.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Cynical'
-
-__username__ = "M477h3w1012"
-__password__ = "Rainbow Twilight Sky"
-__reddit_client__ = 'FfFV8ZzQZcVBGQ'
-__reddit_secret__ = 'iJ3luvkubehx6oVeWXeP58pW9M4'
-__imgur_client__ = '8f7c080324b4da6'
-__imgur_secret__ = '38dad2a936bf26bcd3e958c0c0162293a5e02cf3'
