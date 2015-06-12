402693bdfd80d233a6a588e0f513f06d82f82663
Matthew D. Ball <m.d.ball2@ncl.ac.uk>
2015-06-12 07:57:13 +0100

    Adding in a few changes and general things (might not be final)

--- tests/SteamCrawlerTest.py
+++ tests/SteamCrawlerTest.py
@@ -19,7 +19,7 @@ import unittest
 import xml.etree.ElementTree as ET
 
 from tests import configs
-from walkers.SteamCrawler import SteamCrawler
+from walkers.SteamCrawler import SteamCrawler, WalkerException
 
 
 class SteamCrawlerTest(unittest.TestCase):
@@ -27,17 +27,30 @@ class SteamCrawlerTest(unittest.TestCase):
     def setUp(self):
         self.sc = SteamCrawler(configs.__steam_api_key__)
 
-    def testGetMain(self):
+    def testGetMethods(self):
+        self.assertEqual(50, len(self.sc.getAvailableMethods()), "Not enough interfaces were returned")
+
+    def testGetMainLeaderboard(self):
         self.sc.assignGame(247080)
         et = ET.fromstring(self.sc.getMainLeaderboard())
         print(et)
 
     def testGetProfile(self):
-        res = self.sc.getDataOnProfileId(configs.__steam_test_profile__)
+        res = self.sc.accessSteam('ISteamUser', 'GetPlayerSummaries', 2, {'steamids': configs.__steam_test_profile__})
         self.assertEqual(
             configs.__steam_test_profile__, res['response']['players'][0]['steamid'],
             "Slight issue in the fact that we don't have the right person"
         )
+
+    def testFailGetProfile(self):
+        try:
+            self.sc.accessSteam('ISteamUser', 'GetPlayerSummaries', 2, {})
+        except WalkerException as e:
+            self.assertEqual(1, 1, "We failed correctly")
+        except:
+            self.fail("The wrong exception was thrown")
+        else:
+            self.fail("Hrm... This test should fail")
 
 
 if __name__ == '__main__':
--- walkers/SteamCrawler.py
+++ walkers/SteamCrawler.py
@@ -13,32 +13,123 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+
 __author__ = 'Matthew Ball'
-__base__ = "https://steamcommunity.com/stats/{0}/leaderboards/{1}/?xml=1"
-__profile__ = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}"
+
+__base__ = 'https://api.steampowered.com/{0}/{1}/v{2:04d}/'
+
+__auth__ = __base__.format('ISteamWebAPIUtil', 'GetSupportedAPIList', 1)+'?key={0}'
+__leaderboard__ = 'https://steamcommunity.com/stats/{0}/leaderboards/{1}/?xml=1'
+
+__news__ = 'http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/' \
+           '?appid={0}&count={1}&maxlength={2}&format={3}'
+__g_ach__ = 'http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/' \
+            '?gameid={0}&format={1}'
+__g_stats__ = 'http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/' \
+              '?appid={0}&count={1}&name[0]={2}&format={3}'
+__profile__ = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/' \
+              '?key={0}&steamids={1}&format={2}'
+__friends__ = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/' \
+              '?key={0}&steamid={1}&relationship={2}&format={3}'
+__p_ach__ = 'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/' \
+            '?key={0}&appid={1}&steamid={2}&format={3}'
+__p_stats__ = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/' \
+              '?key={0}&appid={1}&steamid={2}&format={3}'
+__games__ = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/' \
+            '?key={0}&steamid={1}&format={2}'
+__r_games__ = 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/' \
+              '?key={0}&steamid={1}&format={2}'
+__shared__ = 'http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/' \
+             '?key={0}&steamid={1}&appid_playing={2}&format={3}'
+__schema__ = 'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/' \
+             '?key={0}&appid={1}&format={2}'
+__bans__ = 'http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/' \
+           '?key={0}&steamids={1}&format={2}'
 
 import requests
 
 
-class SteamCrawler:
+class WalkerException(Exception):
+    def __init__(self, reason):
+        self.reason = reason
 
+    def __str__(self):
+        return repr(self.reason)
+
+class SteamCrawler:
     game = 0
 
     def __init__(self, apiKey):
         self.headers = {"User-Agent": "other:walkers:v0.0.1 (by /u/M477h3w1012)"}
         self.apiKey = apiKey
+        self.interfaces = {}
+        t_map = requests.get(__auth__.format(apiKey), headers=self.headers).json()['apilist']['interfaces']
+        for interface in t_map:
+            mtemp, temp, t, m = ({},)*4
+            for method in interface['methods']:
+                t[method['version']] = method
+                temp.update(t)
+                m[method['name']] = temp
+                mtemp.update(m)
+            self.interfaces[interface['name']] = mtemp
+
+    def checkAccess(self, interface, method, version):
+        return \
+            interface in self.interfaces and \
+            method in self.interfaces[interface] and \
+            version in self.interfaces[interface][method]
+
+    def requiresKey(self, interface, method, version):
+        for param in self.interfaces[interface][method][version]['parameters']:
+            if param['name'] == 'key':
+                return True
+        return False
+
+    def checkParameterSet(self, interface, method, version, parameters):
+        for param in self.interfaces[interface][method][version]['parameters']:
+            if not param['optional']:
+                if not param['name'] in parameters:
+                    raise WalkerException(
+                        'Non-Optional Parameter Missing! {0} ({1}): {2}'.format(
+                            param['name'], param['type'], param['description']
+                        )
+                    )
+        return True
+
+    def accessSteam(self, interface, method, version, parameters):
+        try:
+            if self.checkAccess(interface, method, version):
+                if self.requiresKey(interface, method, version):
+                    parameters['key'] = self.apiKey
+                if self.checkParameterSet(interface, method, version, parameters):
+                    ret = requests.get(
+                        __base__.format(
+                            interface, method, version,
+                        ), headers=self.headers, params=parameters
+                    )
+                    if 'format' in parameters:
+                        if parameters['format'] == 'json':
+                            return ret.json()
+                        else:
+                            return ret.content
+                    return ret.json()
+        except:
+            raise
+
+    def getAvailableMethods(self):
+        return self.interfaces
 
     def assignGame(self, id):
         self.game = id
 
     def getMainLeaderboard(self):
-        res = requests.get(__base__.format(self.game, ""), headers=self.headers)
+        res = requests.get(__leaderboard__.format(self.game, ""), headers=self.headers)
         return res.content
 
     def getIndivLeaderboard(self, ind):
-        res = requests.get(__base__.format(self.game, ind), headers=self.headers)
+        res = requests.get(__leaderboard__.format(self.game, ind), headers=self.headers)
         return res.content
 
     def getDataOnProfileId(self, profileId):
-        res = requests.get(__profile__.format(self.apiKey, profileId), headers=self.headers)
+        res = requests.get(__profile__.format(self.apiKey, profileId, 'json'), headers=self.headers)
         return res.json()
